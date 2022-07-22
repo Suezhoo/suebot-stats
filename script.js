5bb2f6f67fd3076a6c9e552459344e4b913b790d
Suezhoo <suezhoo@outlook.com>
2022-07-22 22:41:53 +0200

    [DONE] Styling + inserting html

--- script.js
+++ script.js
@@ -18,22 +18,28 @@ async function init() {
             const senders = [];
 
             data.chats.forEach((c) => {
+                const message = { value: c.message, streamState: c.streamState, streamer: c.target };
+
                 const chat = {
                     sender: c.sender,
-                    messages: [{ value: c.message, streamState: c.streamState }],
-                    streamer: c.target,
+                    online_messages: [],
+                    offline_messages: [],
                     date: c.date,
+                    all_messages: [message],
                 };
+
+                c.streamState === "online" ? chat.online_messages.push(message) : chat.offline_messages.push(message);
 
                 const sender = senders.find((sender) => sender.sender === c.sender);
                 if (!sender) {
                     senders.push(chat);
                 } else {
-                    sender.messages.push({ value: c.message, streamState: c.streamState });
+                    c.streamState === "online" ? sender.online_messages.push(message) : sender.offline_messages.push(message);
+                    sender.all_messages.push(message);
                 }
             });
             // Sort by message count and pass through
-            updateHtml(senders.sort((a, b) => a.messages.length + b.messages.length));
+            updateHtml(senders);
         });
 }
 
@@ -42,20 +48,26 @@ function updateHtml(data) {
 }
 
 function displayTopChatters(data) {
-    console.log(data);
     const top_chatters_container = document.querySelector("#top-chatters-container");
     const top_online_chatters_container = document.querySelector("#top-online-chatters-container");
     const top_offline_chatters_container = document.querySelector("#top-offline-chatters-container");
 
-    data.forEach((chatter) => {
-        // TOP LIFETIME CHATTERS
-        top_chatters_container.innerHTML += `<p>${chatter.sender} | ${chatter.messages.length}</p>`;
-        // TOP ONLINE CHATTERS
-        const online_messages = chatter.messages.filter((message) => (message.streamState === "online" ? true : false));
-        if (online_messages.length > 0) top_online_chatters_container.innerHTML += `<p>${chatter.sender} | ${online_messages.length}</p>`;
-        // TOP OFFLINE CHATTERS
-        const offline_messages = chatter.messages.filter((message) => (message.streamState === "offline" ? true : false));
-        if (offline_messages.length > 0) top_offline_chatters_container.innerHTML += `<p>${chatter.sender} | ${offline_messages.length}</p>`;
+    // TOP LIFETIME CHATTERS
+    const lifetime_all_chatters = data.sort((a, b) => b.all_messages.length - a.all_messages.length);
+    lifetime_all_chatters.forEach((chatter, index) => {
+        top_chatters_container.innerHTML += `<div><p>${index + 1}</p><p>${chatter.sender}</p><p>${chatter.all_messages.length}</p></div>`;
+    });
+
+    const lifetime_online_chatters = data.sort((a, b) => b.online_messages.length - a.online_messages.length);
+    lifetime_online_chatters.forEach((chatter, index) => {
+        if (chatter.online_messages.length > 0)
+            top_online_chatters_container.innerHTML += `<div><p>${index + 1}</p><p>${chatter.sender}</p><p>${chatter.online_messages.length}</p></div>`;
+    });
+
+    const lifetime_offline_chatters = data.sort((a, b) => b.offline_messages.length - a.offline_messages.length);
+    lifetime_offline_chatters.forEach((chatter, index) => {
+        if (chatter.offline_messages.length > 0)
+            top_offline_chatters_container.innerHTML += `<div><p>${index + 1}</p><p>${chatter.sender}</p><p>${chatter.offline_messages.length}</p></div>`;
     });
 }
 
