b3d3460931bb94328074a1b2896beddcdc52de5b
Suezhoo <suezhoo@outlook.com>
2022-07-24 21:34:29 +0200

    Added monthly chatters leaderboard

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ window.onload = () => {
 };
 
 async function updateHtml() {
-    const top_chatters_container = document.querySelector("#top-chatters-container");
-    const top_online_chatters_container = document.querySelector("#top-online-chatters-container");
-    const top_offline_chatters_container = document.querySelector("#top-offline-chatters-container");
+    await displayLifetimeStats();
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+    await displayMonthlyStats(month, year);
+}
 
-    const total_messages = document.querySelector("#total-lifetime-messages");
+async function displayLifetimeStats() {
+    // DOM VARS
+    const total_lifetime_messages = document.querySelector("#total-lifetime-messages");
+    const top_lifetime_chatters_container = document.querySelector("#top-lifetime-chatters-container");
+    const top_lifetime_online_chatters_container = document.querySelector("#top-lifetime-online-chatters-container");
+    const top_lifetime_offline_chatters_container = document.querySelector("#top-lifetime-offline-chatters-container");
 
     await fetch(`${url}/chatters?streamState=all`, {
         method: "get",
@@ -22,26 +29,85 @@ async function updateHtml() {
         .then((res) => res.json())
         .then((data) => {
             if (data.error) {
-                total_messages.textContent = 0;
+                total_lifetime_messages.textContent = 0;
+                console.log(data.error);
                 return;
             }
-            total_messages.textContent = data.total_all_messages;
+            // STOP LOADING
+            top_lifetime_chatters_container.innerHTML = "";
+            top_lifetime_online_chatters_container.innerHTML = "";
+            top_lifetime_offline_chatters_container.innerHTML = "";
+
+            total_lifetime_messages.textContent = data.total_all_messages;
             //
             if (data.all_chat.length > 0) {
                 const top_chatters = data.all_chat.sort((a, b) => b.messages.length - a.messages.length);
                 top_chatters.forEach((chatter, index) => {
-                    top_chatters_container.innerHTML += `<div>
-                    <p>${index + 1}</p>
-                    <p>${chatter.sender}</p>
-                    <p>${chatter.messages.length}</p>
-                    </div>`;
+                    top_lifetime_chatters_container.innerHTML += `<div>
+                <p>${index + 1}</p>
+                <p>${chatter.sender}</p>
+                <p>${chatter.messages.length}</p>
+                </div>`;
                 });
             }
             //
             if (data.online_chat.length > 0) {
                 const top_online_chatters = data.online_chat.sort((a, b) => b.messages.length - a.messages.length);
                 top_online_chatters.forEach((chatter, index) => {
-                    top_online_chatters_container.innerHTML += `<div>
+                    top_lifetime_online_chatters_container.innerHTML += `<div>
+                <p>${index + 1}</p>
+                <p>${chatter.sender}</p>
+                <p>${chatter.messages.length}</p>
+                </div>`;
+                });
+            }
+            //
+            if (data.offline_chat.length > 0) {
+                const top_offline_chatters = data.offline_chat.sort((a, b) => b.messages.length - a.messages.length);
+                top_offline_chatters.forEach((chatter, index) => {
+                    top_lifetime_offline_chatters_container.innerHTML += `<div>
+                <p>${index + 1}</p>
+                <p>${chatter.sender}</p>
+                <p>${chatter.messages.length}</p>
+                </div>`;
+                });
+            }
+        })
+        .catch((err) => {
+            console.log("Caught an error:", err);
+        });
+}
+
+async function displayMonthlyStats(month, year) {
+    // DOM VARS
+    const monthly_messages_text = document.querySelector("#monthly-messages-text");
+    const total_monthly_messages = document.querySelector("#total-monthly-messages");
+    const top_monthly_all_chatters_container = document.querySelector("#top-monthly-chatters-container");
+    const top_monthly_online_chatters_container = document.querySelector("#top-monthly-online-chatters-container");
+    const top_monthly_offline_chatters_container = document.querySelector("#top-monthly-offline-chatters-container");
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const monthName = months[month];
+
+    monthly_messages_text.textContent = `${monthName.toUpperCase()}'S MONTHLY MESSAGES`;
+
+    await fetch(`${url}/chats/filter?month=${month + 1}&year=${year}`)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.error) {
+                total_monthly_messages.textContent = 0;
+                console.log(data);
+                return;
+            }
+            total_monthly_messages.textContent = data.total_all_messages;
+            // STOP LOADING
+            top_monthly_all_chatters_container.innerHTML = "";
+            top_monthly_online_chatters_container.innerHTML = "";
+            top_monthly_offline_chatters_container.innerHTML = "";
+            //
+            if (data.monthly_all_chat.length > 0) {
+                data.monthly_all_chat.forEach((chatter, index) => {
+                    top_monthly_all_chatters_container.innerHTML += `<div>
                     <p>${index + 1}</p>
                     <p>${chatter.sender}</p>
                     <p>${chatter.messages.length}</p>
@@ -49,15 +115,27 @@ async function updateHtml() {
                 });
             }
             //
-            if (data.offline_chat.length > 0) {
-                const top_offline_chatters = data.offline_chat.sort((a, b) => b.messages.length - a.messages.length);
-                top_offline_chatters.forEach((chatter, index) => {
-                    top_offline_chatters_container.innerHTML += `<div>
+            if (data.monthly_online_chat.length > 0) {
+                data.monthly_online_chat.forEach((chatter, index) => {
+                    top_monthly_online_chatters_container.innerHTML += `<div>
                     <p>${index + 1}</p>
                     <p>${chatter.sender}</p>
                     <p>${chatter.messages.length}</p>
                     </div>`;
                 });
             }
+            //
+            if (data.monthly_offline_chat.length > 0) {
+                data.monthly_offline_chat.forEach((chatter, index) => {
+                    top_monthly_offline_chatters_container.innerHTML += `<div>
+                    <p>${index + 1}</p>
+                    <p>${chatter.sender}</p>
+                    <p>${chatter.messages.length}</p>
+                    </div>`;
+                });
+            }
+        })
+        .catch((err) => {
+            console.log("Caught an error:", err);
         });
 }
